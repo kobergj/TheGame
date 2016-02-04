7863e7fc22db6356b01a8967808cf1114f04aa38
kobergj <julian.koberg@windeln.de>
2016-02-04 21:51:25 +0100

    planets

---
+++ models/anomalies.py
@@ -0,0 +1,16 @@
+# Contains Planet Data
+
+
+class Anomaly():
+    # Generic Anomaly in Space.
+    # TODO: Coordinates
+    def __init__(self):
+        self.coordinates = [0, 0, 0]
+
+
+class Planet(Anomaly):
+    def __init__(self, planetInformation):
+        Anomaly.__init__(self)
+
+        self.goodsConsumed = planetInformation['goodsConsumed']
+        self.goodsProduced = planetInformation['goodsProduced']
